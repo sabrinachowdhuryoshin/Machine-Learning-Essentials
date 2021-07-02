47ad916bce9461d79f475c144d182530c7c1ba0e
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-07-02 02:20:40 +0200

    file is updated

--- linear_regression01.py
+++ linear_regression01.py
@@ -1,7 +1,19 @@
+#linear regression on google stocks
+
 import pandas as pd
 
 #read the csv data
 df = pd.read_csv("GOOG.csv")  
 
-#print the csv data
+#chosen important features
+df = df[['adjOpen','adjClose','adjVolume','adjHigh','adjLow']]
+
+#finding the percentage of imortant features
+df['HL_PCT'] = (df['adjHigh'] - df['adjLow']) / df['adjClose'] *100
+
+#finding the daily percent change
+df['PCT_change'] = (df['adjClose'] - df['adjOpen']) / df['adjOpen'] *100
+
+#choosing the important parameters to print
+df = df [['adjClose', 'HL_PCT', 'PCT_change', 'adjVolume']]
 print(df.head())
